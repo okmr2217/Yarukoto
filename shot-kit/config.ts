17e181ebd10fb3dde6e5bb858ff961ce8f0f6ce0
okmr2217 <o.yamaneko0331@gmail.com>
2026-04-28 21:23:59 +0900

    chore: shot-kitのauth-handler、config、scenariosを削除

--- shot-kit/config.ts
+++
@@ -1,8 +0,0 @@
-import type { ShotKitProjectConfig } from 'shot-kit';
-
-const config: ShotKitProjectConfig = {
-  projectName: 'yarukoto',
-  baseUrl: process.env['SHOT_KIT_BASE_URL'] ?? 'http://localhost:3000',
-};
-
-export default config;
